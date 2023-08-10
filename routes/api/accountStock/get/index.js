2b895d811f58738e598b5015dcf8dbb8e2b0344b
apolo-sys <cristian124421@gmail.com>
2023-08-10 05:00:08 +0200

    Added accountsStock get/add route

---
+++ routes/api/accountStock/get/index.js
@@ -0,0 +1,59 @@
+'use strict'
+
+module.exports = async function (fastify, opts) {
+    const JWTModel = require('../../../../models/JWT')
+    const UserModel = require('../../../../models/User')
+    const jwt = new JWTModel()
+    const { nanoid } = require('nanoid')
+
+    fastify.get('/', async function (request, reply) {
+        let conn
+
+        const { auth_token,} = request.query
+
+        if (!auth_token) {
+            return reply.code(400).send({
+                message:
+                    'Need to send: auth_token',
+            })
+        }
+
+        try {
+            conn = await request.dbpool.getConnection()
+
+            const validToken = await jwt.verifyTokenWithAuth(
+                auth_token,
+                'user_auth'
+            )
+
+            if (validToken?.valid === false)
+                return reply.code(400).send(validToken)
+
+            const user = new UserModel(validToken.userId, request.dbpool)
+
+            const userData = await user.getUserData()
+
+            if (userData?.valid === false) return reply.code(400).send(userData)
+
+            if (userData?.rank !== 'admin') {
+                return reply.code(400).send({
+                    message: 'Only admins can get stock',
+                })
+            }
+
+            const rows = await conn.query(
+                'SELECT * FROM stock_accounts',
+            )
+
+            if (rows.length < 0) {
+                return reply.code(400).send({
+                    message: 'There is no stock left',
+                })
+            }
+
+            return { valid: true, rows }
+        } finally {
+            if (conn) conn.release()
+        }
+    })
+}
